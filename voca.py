94f57a474c86c01b03d38082bd102ece49efa44d
ZheyuanFeng21 <90481258+ZheyuanFeng21@users.noreply.github.com>
2021-11-07 10:15:57 -0600

    Update voca.py

--- voca.py
+++ voca.py
@@ -1,10 +1,13 @@
+import re
+
 vocab = set()
+prefixes = ('#', '@', 'https')
 
 for tweet in open("tweets.txt", "r").readlines():
-	_, _, text = tweet.partition(', ')	
-	words = list(map(lambda w: w.lower(), text.split()))
+	_, _, text = tweet.partition(', ')
+	terms = text.strip().split()
+	terms = [word for word in terms if not word.startswith(prefixes)]
+	words = [re.sub(r'\W+', '', word).lower() for word in terms]
 	vocab.update(set(words))
 
-
-print("The unique words")
-print(vocab)
+print(f"There are '{len(vocab)}' unique words")
